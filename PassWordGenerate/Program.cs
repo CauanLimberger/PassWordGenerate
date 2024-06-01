817d0898197fa4880fd8cb25e8140ad4c4661690
CauanLimberger <cauanlimberger@gmail.com>
2024-05-31 22:13:01 -0300

    Adicionar arquivos de projeto.

---
+++ PassWordGenerate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace PassWordGenerate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int rdn = random.Next(2);
+            int tamanhoSenha = (rdn == 0) ? 16 : 14;
+            string caracteresBox = "ABCDEFGHIJKLNIOPQRSTUVWXYZ<>:?!@#$%*()_+^}{";
+            StringBuilder senha = new StringBuilder();
+            for (int cont = 0; cont < tamanhoSenha; cont++)
+            {
+                int indiceCaixa = random.Next(0, caracteresBox.Length -1);
+                senha.Append(caracteresBox[indiceCaixa]);
+            }
+            Console.WriteLine(senha.ToString());
+        }
+    }
+}
